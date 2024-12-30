d122c500938a28f2e89ba104d4af588c2a0ce049
BenGutteridge <benpgutteridge@gmail.com>
2024-12-30 14:27:43 +0000

    update panopticon for Xmas period and new year

--- _posts/make_empty_posts.py
+++ _posts/make_empty_posts.py
@@ -1,11 +1,12 @@
 """
-Usage: python script_name.py YYYY-MM-DD YYYY-MM-DD
+Usage: python make_empty_posts.py YYYY-MM-DD YYYY-MM-DD
 To create empty files from -> to (inclusive)
 """
 
 import os
 import sys
 from datetime import datetime, timedelta
+
 
 def generate_markdown_files(start_date_str, end_date_str):
     # Define the template
@@ -25,8 +26,8 @@ tags:
 
 """
     # Convert the string arguments to datetime objects
-    start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
-    end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
 
     # Iterate over each day between the start and end date
     current_date = start_date
@@ -34,9 +35,9 @@ tags:
         # Check if the current date is a weekday (Monday=0, ..., Friday=4)
         if current_date.weekday() < 5:
             # Format the date strings for the file and content
-            formatted_date = current_date.strftime('%Y-%m-%d')
-            permalink = current_date.strftime('%Y/%m/%d')
-            filename = current_date.strftime('%Y-%m-%d') + '-.md'
+            formatted_date = current_date.strftime("%Y-%m-%d")
+            permalink = current_date.strftime("%Y/%m/%d")
+            filename = current_date.strftime("%Y-%m-%d") + "-.md"
 
             # Check if the file already exists
             if not os.path.exists(filename):
@@ -44,17 +45,18 @@ tags:
                 content = template.format(date=formatted_date, permalink=permalink)
 
                 # Write the content to the corresponding file
-                with open(filename, 'w') as f:
+                with open(filename, "w") as f:
                     f.write(content)
-                
-                print(f'Generated: {filename}')
+
+                print(f"Generated: {filename}")
             else:
-                print(f'Skipped (already exists): {filename}')
+                print(f"Skipped (already exists): {filename}")
 
         # Move to the next day
         current_date += timedelta(days=1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Check if the correct number of arguments are provided
     if len(sys.argv) != 3:
         print("Usage: python script_name.py YYYY-MM-DD YYYY-MM-DD")
